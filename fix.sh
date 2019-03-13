deef8f18f51b42c0942f460ade56f1296fb663c3
Ubuntu <azureuser@accVM.stxqiph3xlqupe45tdcx343pyd.bx.internal.cloudapp.net>
2019-03-13 22:02:14 +0000

    Remove host generated files

--- fix.sh
+++
@@ -1,5 +0,0 @@
-chmod 777 /home/azureuser/openenclave-install4/include/openenclave/bits/timetypes.h
-chmod 777 /home/azureuser/openenclave-install4/include/openenclave/3rdparty/libc/bits/alltypes.h
-cp ./timetypes.h  /home/azureuser/openenclave-install4/include/openenclave/bits/timetypes.h
-cp ./alltypes.h /home/azureuser/openenclave-install4/include/openenclave/3rdparty/libc/bits/alltypes.h
-cp  ~/oe/include/openenclave/socket.edl ./socket.edl
